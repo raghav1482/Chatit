e2c148360da5b1d8561bb49e69664a02e18dedb8
Raghvendra Tiwari <98442936+raghav1482@users.noreply.github.com>
2024-01-22 00:55:15 +0530

    Update soketservice.js

--- mychatapp/src/components/soketservice.js
+++ mychatapp/src/components/soketservice.js
@@ -1,7 +1,7 @@
 // socketService.js
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://chatitserver.onrender.com');
 
 const connectToSocket = (userId) => {
     socket.io.opts.query = { userId };
